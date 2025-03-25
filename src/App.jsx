e457783fcefc3ac7ac88543947b8880607f9ea8a
julia <julia.iervese@aln.senaicimatec.edu.br>
2025-03-25 13:31:00 -0300

    Corrigindo envio dos arquivos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CalculadoraIdade from "./components/CalculadoraIdade";
+
+function App() {
+    return (
+        <div>
+            <CalculadoraIdade />
+        </div>
+    );
+}
+
+export default App;
